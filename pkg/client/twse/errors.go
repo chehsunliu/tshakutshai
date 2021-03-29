070db99580c07eb1a1757b8fa033733b2c1dedac
Che-Hsun Liu <chehsunliu@gmail.com>
2021-03-30 01:19:19 +0800

    :pencil: Add simple documents (#8)

--- pkg/client/twse/errors.go
+++ pkg/client/twse/errors.go
@@ -2,6 +2,9 @@ package twse
 
 import "fmt"
 
+// NoDataError is an error returned by Fetch functions when query conditions matches nothing. It can be
+// no such stock symbol, dates before the stock's IPO, or the TWSE server did not have any data of all
+// the stocks before the query date.
 type NoDataError struct {
 	Message string
 }
@@ -10,6 +13,8 @@ func (e *NoDataError) Error() string {
 	return fmt.Sprintf("NoData: %s", e.Message)
 }
 
+// QuotaExceededError is an error returned by Fetch functions when query the TWSE server too frequently.
+// Typically it takes around 1 hour to get back to normal.
 type QuotaExceededError struct {
 	Message string
 }
@@ -18,6 +23,7 @@ func (e *QuotaExceededError) Error() string {
 	return fmt.Sprintf("QuotaExceeded: %s", e.Message)
 }
 
+// ConnectionError is an error returned by Fetch functions when having problem to connect to the TWSE server.
 type ConnectionError struct {
 	Message string
 }
