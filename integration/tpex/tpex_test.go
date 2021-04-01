527428ebb55f9527cd4df34d85114d2a58a92a37
chehsunliu <chehsunliu@gmail.com>
2021-04-02 01:09:32 +0800

    :white_check_mark: Add integration test for TPEx daily quotes

--- integration/tpex/tpex_test.go
+++ integration/tpex/tpex_test.go
@@ -42,3 +42,33 @@ func TestClient_FetchDayQuotes(t *testing.T) {
 	assert.Equal(t, 89.40, q2.Open)
 	assert.Equal(t, 88.00, q2.Close)
 }
+
+func TestClient_FetchDailyQuotes(t *testing.T) {
+	qs, err := client.FetchDailyQuotes("8044", 2020, time.March)
+	assert.Nilf(t, err, "%+v", err)
+	assert.Equal(t, 22, len(qs))
+
+	q0 := qs[0]
+	assert.Equal(t, "8044", q0.Code)
+	assert.Equal(t, "網家", q0.Name)
+	assert.Equal(t, "20200302", q0.Date.Format("20060102"))
+	assert.Equal(t, uint64(815_000), q0.Volume)
+	assert.Equal(t, uint64(670), q0.Transactions)
+	assert.Equal(t, uint64(84_369_000), q0.Value)
+	assert.Equal(t, 105.50, q0.High)
+	assert.Equal(t, 101.00, q0.Low)
+	assert.Equal(t, 104.50, q0.Open)
+	assert.Equal(t, 102.50, q0.Close)
+
+	q1 := qs[21]
+	assert.Equal(t, "8044", q1.Code)
+	assert.Equal(t, "網家", q1.Name)
+	assert.Equal(t, "20200331", q1.Date.Format("20060102"))
+	assert.Equal(t, uint64(875_000), q1.Volume)
+	assert.Equal(t, uint64(750), q1.Transactions)
+	assert.Equal(t, uint64(61_249_000), q1.Value)
+	assert.Equal(t, 71.50, q1.High)
+	assert.Equal(t, 69.00, q1.Low)
+	assert.Equal(t, 70.90, q1.Open)
+	assert.Equal(t, 70.30, q1.Close)
+}
